5afd9a7ad61eb94078b477bfd3d1c7f69b079f08
axie10 <cryptosushi21@gmail.com>
2024-08-01 14:17:29 +0200

    feat: Add formulario-shared component and update home component
    - Add formulario-shared component to display a form with input fields and validation
    - Update home component to include the formulario-shared component
    - Update styles for the home component to center the form

--- src/app/pages/paginaFormulario/home/home.component.ts
+++ src/app/pages/paginaFormulario/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormularioSharedComponent } from '../../../shared/components/formulario-shared/formulario-shared.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true,
-  imports: []
+  imports: [FormularioSharedComponent]
 })
 export class HomeComponent implements OnInit {
 
--- src/app/shared/components/formulario-shared/formulario-shared.component.ts
+++ src/app/shared/components/formulario-shared/formulario-shared.component.ts
@@ -1,17 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
 
 @Component({
   selector: 'app-formulario-shared',
   templateUrl: './formulario-shared.component.html',
   styleUrls: ['./formulario-shared.component.scss'],
   standalone: true,
-  imports: []
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, ReactiveFormsModule, CommonModule]
 })
 export class FormularioSharedComponent implements OnInit {
+
+  public fb = inject(FormBuilder);
+
+  public type = ['human', 'Alien', 'Humanoid', 'Cronenberg', 'unknown'];
+
+  public FormularioShared : FormGroup = this.fb.group({
+    name: ['', Validators.required],
+    email: ['',[Validators.required, Validators.email]],
+    personajes: this.fb.group({
+      type: [''],
+      nombre: [''],
+    }),
+  })
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  enviar(){
+  }
+
+  get personajes(){
+    return this.FormularioShared.get('personajes') as FormGroup;
+  }
+
+  get email(){
+    return this.FormularioShared.get('email') as FormGroup;
   }
 
 }
